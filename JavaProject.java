1aed8985cc2f4fcaab1376c8d3651205d079c8d1
JemalJaquavius <acrause@learn68.ca>
2025-05-31 19:13:23 -0700

    Update JavaProject.java

--- JavaProject.java
+++ JavaProject.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
-package empty.javaproject;
-
-/**
- *
- * @author Atticus
- */
 import java.util.Scanner;
 public class JavaProject {
     String information = "Buzz Lightyear is great";
