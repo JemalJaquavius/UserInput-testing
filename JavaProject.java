9c3848e1553bc933ab3edd8232e1ae3d6602ed30
JemalJaquavius <acrause@learn68.ca>
2025-05-31 19:13:01 -0700

    Add files via upload

---
+++ JavaProject.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package empty.javaproject;
+
+/**
+ *
+ * @author Atticus
+ */
+import java.util.Scanner;
+public class JavaProject {
+    String information = "Buzz Lightyear is great";
+
+    public static void main(String[] args) {
+    Scanner nameOfUser = new Scanner(System.in);
+    System.out.println("enter username:");
+
+    String username = nameOfUser.nextLine();
+    System.out.println("""
+                       
+                       Welcome,
+                       """ + username);
+    
+    
+    
+    Scanner Numbers = new Scanner(System.in);
+    System.out.println("""
+                       
+                       Enter two numbers:
+                       """);
+    int number = Numbers.nextInt();
+    int number2 = Numbers.nextInt();
+    
+
+    System.out.println(number + " plus " + number2 + " is equal to:");
+    System.out.println(number + number2);
+    
+    JavaProject reallyReal = new JavaProject();
+    System.out.println(reallyReal.information);
+    
+    uselessCode newObj = new uselessCode();
+    System.out.println(newObj.text);
+    
+    info soon = new info();
+    System.out.println(soon.ending);
+    }
+
+    private static class uselessCode {
+     String text;
+     String elseText;
+    
+    public uselessCode() {
+    if (5 + 2 == 7) {
+      text = """
+             
+             It's true.
+             
+             Trust me.
+             """;
+    } else {
+    elseText = """
+               It isn't true.
+               
+               My bad, guys.
+               """;
+    }
+    }
+        
+    }
+}
---
+++ secondaryFile.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package empty.javaproject;
+
+/**
+ *
+ * @author Atticus
+ */
+public class secondaryFile {
+    
+}
+class info {
+    String ending = "Adding more soon";
+}
